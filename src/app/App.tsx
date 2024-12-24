dc8a2bf1a1771da2669e51b87bf8047d349122a9
Vadim Tatarintsev <v.tatarincev@astondevs.ru>
2024-12-24 14:59:18 +0300

    handle empty route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,22 @@
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
+
 import { HomePage } from "@pages/HomePage"
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		index: true,
+		element: <HomePage />,
+		errorElement: <Navigate to={"/"} />,
+	},
+	{
+		path: "*",
+		element: <Navigate to={"/"} />,
+	},
+])
+
 function App() {
-	return (
-		<>
-			<HomePage />
-		</>
-	)
+	return <RouterProvider router={router} />
 }
 
 export default App
