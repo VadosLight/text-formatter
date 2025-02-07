56830c97a615026a37885760110c832c47d6a118
Vadim Tatarintsev <v.tatarincev@astondevs.ru>
2025-02-07 21:32:54 +0300

    Updates

---
+++ assets/HomePage-BcF9_BAI.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BTJtw0Ed.js";import{P as e}from"./PageWrapper-Ca1jILR5.js";const a=()=>r.jsx(e,{children:"Главная страничка"});export{a as default};
--- assets/HomePage-Bx_WuxTn.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-GqqHLrW0.js";import{P as e}from"./PageWrapper-BRGP8_Qi.js";const a=()=>r.jsx(e,{children:"Главная страничка"});export{a as default};
