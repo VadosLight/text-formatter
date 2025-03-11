1152e52f9b967c5b1e294e75cf91f5b1e7241965
Vadim Tatarintsev <v.tatarincev@astondevs.ru>
2025-03-11 12:35:34 +0300

    Update json-escape.test.ts

--- src/features/object-to-escaped-json/lib/json-escape.test.ts
+++ src/features/object-to-escaped-json/lib/json-escape.test.ts
@@ -83,138 +83,200 @@ describe("escapeJinjaJson", () => {
 describe("escapeJinjaJson - сложный случай", () => {
     it("должен экранировать Jinja теги в большом количестве", () => {
         expect(escapeJinjaJson(`{
-    "header": {
-        "type": "default",
-        "title": ""
-    },
-    "content": {
-        "type": "list",
-        "items": [
-            {
-                "type": "SpacingView",
-                "content": {
-                    "size": "xl6"
-                }
-            },
-            {
-                "type": "SpacingView",
-                "content": {
-                    "size": "xl6"
-                }
-            },
-            {
-                "type": "SpacingView",
-                "content": {
-                    "size": "xl6"
-                }
-            },
-            {
-                "type": "StackWrapper",
-                "content": {
-                    "axis": "horizontal",
-                    "horizontalAlignment": "center",
-                    "padding": {
-                        "top": "xl",
-                        "left": "s",
-                        "right": "s"
-                    },
-                    "children": [
-                        {
-                            "type": "IconView",
-                            "content": {
-                                "icon": {
-                                    "name": "object_party-popper"
-                                },
-                                "size": "xLarge",
-                                "shape": "noShape"
-                            }
-                        }
-                    ]
-                }
-            },
-            {
-                "type": "TextLabel",
-                "content": {
-                    "text": "Теперь у родителя есть доступ к счёту",
-                    "color": "textPrimary",
-                    "typography": "HeadlineLarge",
-                    "horizontalLayout": "fill",
-                    "alignment": "center",
-                    "padding": {
-                        "top": "xl",
-                        "bottom": "s",
-                        "left": "horizontalMargin",
-                        "right": "horizontalMargin"
-                    }
-                }
-            },
-            {
-                "type": "TextLabel",
-                "content": {
-                    "text": "Отключить доступ можно в настройках портфеля",
-                    "color": "textPrimary",
-                    "typography": "BodyPrimaryMedium",
-                    "horizontalLayout": "fill",
-                    "alignment": "center",
-                    "lineLimit": 2,
-                    "padding": {
-                        "bottom": "s",
-                        "left": "horizontalMargin",
-                        "right": "horizontalMargin"
-                    }
-                }
-            }
-        ]
-    },
-    "footer": {
-        "type": "StackWrapper",
+  "header": {
+    "type": "default",
+    "title": "",
+    "leftButton": "back"
+  },
+  "content": {
+    "type": "list",
+    "items": [
+      {
+        "type": "TextLabel",
         "content": {
-            "axis": "vertical",
-            "padding": {
-                "top": "m",
-                "left": "s",
-                "bottom": "m",
-                "right": "s"
-            },
-            "children": [
-                {% if not is_full_parent_access %
-                }
-        {
-                    "type": "ButtonView",
-                    "content": {
-                        "text": "Добавить второго родителя",
-                        "size": "large",
-                        "style": "primary",
-                        "action": {
-                            "type": "deeplink",
-                            "url": "/portfolio/parent-control/add-parent"
-                        }
-                    }
+          "text": "Доступ родителям",
+          "typography": "HeadlineMedium",
+          "padding": {
+            "top": "xl",
+            "right": "l",
+            "left": "l",
+            "bottom": "s"
+          }
+        }
+      },
+      {
+        "type": "TextLabel",
+        "content": {
+          "text": "Видят баланс вашего брокерского счёта, историю операций, заявки, состав портфеля и аналитику в своём приложении",
+          "typography": "BodyPrimaryMedium",
+          "padding": {
+            "right": "l",
+            "left": "l",
+            "bottom": "s"
+          }
+        }
+      },
+      {
+        "type": "SpacingView",
+        "content": {
+          "size": "xs"
+        }
+      },
+      {% for parent in parents % }
+      {
+        "type": "BannerWrapper",
+        "content": {
+          "padding": {
+            "left": "horizontalMargin",
+            "right": "horizontalMargin",
+            "top": "none"
+          },
+          "content": {
+            "type": "StackWrapper",
+            "content": {
+              "alignment": "center",
+              "axis": "vertical",
+              "horizontalAlignment": "center",
+              "distribution": "fill",
+              "verticalAlignment": "center",
+              "children": [
+                {
+                  "type": "SpacingView",
+                  "content": {
+                    "size": "m"
+                  }
                 },
                 {
-                    "type": "SpacingView",
-                    "content": {
-                        "size": "m"
-                    }
+                  "type": "IconView",
+                  "content": {
+                    "icon": {
+                      "name": "{{ parent.icon | default('face_woman') }}"
+                    },
+                    "shape": "noShape",
+                    "size": "large"
+                  }
                 },
-                {% endif %
-                }
-        {
-                    "type": "ButtonView",
-                    "content": {
-                        "text": "На главный",
-                        "size": "large",
-                        "style": "secondary",
-                        "action": {
-                            "type": "event",
-                            "eventType": "close"
-                        }
+                {
+                  "type": "SpacingView",
+                  "content": {
+                    "size": "m"
+                  }
+                },
+                {
+                  "type": "TextLabel",
+                  "content": {
+                    "text": "{{ parent.name }}"
+                  }
+                },
+                {
+                  "type": "SpacingView",
+                  "content": {
+                    "size": "s"
+                  }
+                },
+                {
+                  "type": "TextLabel",
+                  "content": {
+                    "text": "Имеет доступ",
+                    "color": "textPositive",
+                    "typography": "BodyPrimarySmall"
+                  }
+                },
+                {
+                  "type": "SpacingView",
+                  "content": {
+                    "size": "l"
+                  }
+                },
+                {
+                  "type": "ButtonView",
+                  "content": {
+                    "text": "Отключить доступ",
+                    "style": "secondary",
+                    "icon": {
+                      "name": "glyph_lock-closed_m"
+                    },
+                    "action": {
+                      "type": "http",
+                      "endpoint": "/delete-parent",
+                      "method": "post",
+                      "body": {
+                        "bodyType": "application/json",
+                        "values": [
+                          {
+                            "id": "{{ parent.id }}"
+                          }
+                        ]
+                      },
+                      "analytics": {
+                        "label": "Disable access",
+                        "category": "Parent control",
+                        "dimensions": [],
+                        "action": "Click",
+                        "screen": "List parents"
+                      }
                     }
+                  }
+                },
+                {
+                  "type": "SpacingView",
+                  "content": {
+                    "size": "l"
+                  }
                 }
-            ]
+              ]
+            }
+          },
+          "style": {
+            "backgroundColor": "transparent",
+            "style": "shadow"
+          }
+        },
+        {
+          "type": "SpacingView",
+          "content": {
+            "size": "xs"
+          }
+        },
+      }
+      {% if not loop.last % },
+      {% endif % }
+      {% endfor % }
+    ]
+  },
+  "footer": {
+    "type": "ButtonView",
+    "content": {
+      "padding": {
+        "top": "m",
+        "right": "m",
+        "left": "m",
+        "bottom": "m"
+      },
+      "text": "Добавить родителя",
+      "size": "large",
+      "style": "primary",
+      "action": {
+        "type": "deeplink",
+        "url": "/portfolio/parent-control/add-parent",
+        "analytics": {
+          "label": "Add second parent",
+          "category": "Parent control",
+          "dimensions": [],
+          "action": "Click",
+          "screen": "List parents"
         }
+      }
     }
-}`)).toBe("{\"header\":{\"type\":\"default\",\"title\":\"\",\"leftButton\":\"back\"},\"content\":{\"type\":\"list\",\"items\":[{\"type\":\"TextLabel\",\"content\":{\"text\":\"Доступ родителям\",\"typography\":\"HeadlineMedium\",\"padding\":{\"top\":\"xl\",\"right\":\"l\",\"left\":\"l\",\"bottom\":\"s\"}}},{\"type\":\"TextLabel\",\"content\":{\"text\":\"Видят баланс вашего брокерского счёта, историю операций, заявки, состав портфеля и аналитику в своём приложении\",\"typography\":\"BodyPrimaryMedium\",\"padding\":{\"right\":\"l\",\"left\":\"l\",\"bottom\":\"s\"}}},{\"type\":\"SpacingView\",\"content\":{\"size\":\"xs\"}},{%forparentinparents%}{\"type\":\"BannerWrapper\",\"content\":{\"padding\":{\"left\":\"horizontalMargin\",\"right\":\"horizontalMargin\",\"top\":\"none\"},\"content\":{\"type\":\"StackWrapper\",\"content\":{\"alignment\":\"center\",\"axis\":\"vertical\",\"horizontalAlignment\":\"center\",\"distribution\":\"fill\",\"verticalAlignment\":\"center\",\"children\":[{\"type\":\"SpacingView\",\"content\":{\"size\":\"m\"}},{\"type\":\"IconView\",\"content\":{\"icon\":{\"name\":\"{{ parent.icon | default('face_woman') }}\"},\"shape\":\"noShape\",\"size\":\"large\"}},{\"type\":\"SpacingView\",\"content\":{\"size\":\"m\"}},{\"type\":\"TextLabel\",\"content\":{\"text\":\"{{ parent.name }}\"}},{\"type\":\"SpacingView\",\"content\":{\"size\":\"s\"}},{\"type\":\"TextLabel\",\"content\":{\"text\":\"Имеет доступ\",\"color\":\"textPositive\",\"typography\":\"BodyPrimarySmall\"}},{\"type\":\"SpacingView\",\"content\":{\"size\":\"l\"}},{\"type\":\"ButtonView\",\"content\":{\"text\":\"Отключить доступ\",\"style\":\"secondary\",\"icon\":{\"name\":\"glyph_lock-closed_m\"},\"action\":{\"type\":\"http\",\"endpoint\":\"/delete-parent\",\"method\":\"post\",\"body\":{\"bodyType\":\"application/json\",\"values\":[{\"id\":\"{{ parent.id }}\"}]},\"analytics\":{\"label\":\"Disable access\",\"category\":\"Parent control\",\"dimensions\":[],\"action\":\"Click\",\"screen\":\"List parents\"}}}},{\"type\":\"SpacingView\",\"content\":{\"size\":\"l\"}}]}},\"style\":{\"backgroundColor\":\"transparent\",\"style\":\"shadow\"}},{\"type\":\"SpacingView\",\"content\":{\"size\":\"xs\"}},}{%ifnotloop.last%},{%endif%}{%endfor%}]},\"footer\":{\"type\":\"ButtonView\",\"content\":{\"padding\":{\"top\":\"m\",\"right\":\"m\",\"left\":\"m\",\"bottom\":\"m\"},\"text\":\"Добавить родителя\",\"size\":\"large\",\"style\":\"primary\",\"action\":{\"type\":\"deeplink\",\"url\":\"/portfolio/parent-control/add-parent\",\"analytics\":{\"label\":\"Add second parent\",\"category\":\"Parent control\",\"dimensions\":[],\"action\":\"Click\",\"screen\":\"List parents\"}}}},\"analytics\":{\"label\":\"Screen list\",\"category\":\"Parent control\",\"dimensions\":[],\"action\":\"Load\",\"screen\":\"List parents\"}}"
+  },
+  "analytics": {
+    "label": "Screen list",
+    "category": "Parent control",
+    "dimensions": [],
+    "action": "Load",
+    "screen": "List parents"
+  }
+}`)).toBe("{\\\"header\\\":{\\\"type\\\":\\\"default\\\",\\\"title\\\":\\\"\\\",\\\"leftButton\\\":\\\"back\\\"},\\\"content\\\":{\\\"type\\\":\\\"list\\\",\\\"items\\\":[{\\\"type\\\":\\\"TextLabel\\\",\\\"content\\\":{\\\"text\\\":\\\"Доступ родителям\\\",\\\"typography\\\":\\\"HeadlineMedium\\\",\\\"padding\\\":{\\\"top\\\":\\\"xl\\\",\\\"right\\\":\\\"l\\\",\\\"left\\\":\\\"l\\\",\\\"bottom\\\":\\\"s\\\"}}},{\\\"type\\\":\\\"TextLabel\\\",\\\"content\\\":{\\\"text\\\":\\\"Видят баланс вашего брокерского счёта, историю операций, заявки, состав портфеля и аналитику в своём приложении\\\",\\\"typography\\\":\\\"BodyPrimaryMedium\\\",\\\"padding\\\":{\\\"right\\\":\\\"l\\\",\\\"left\\\":\\\"l\\\",\\\"bottom\\\":\\\"s\\\"}}},{\\\"type\\\":\\\"SpacingView\\\",\\\"content\\\":{\\\"size\\\":\\\"xs\\\"}},{%forparentinparents%}{\\\"type\\\":\\\"BannerWrapper\\\",\\\"content\\\":{\\\"padding\\\":{\\\"left\\\":\\\"horizontalMargin\\\",\\\"right\\\":\\\"horizontalMargin\\\",\\\"top\\\":\\\"none\\\"},\\\"content\\\":{\\\"type\\\":\\\"StackWrapper\\\",\\\"content\\\":{\\\"alignment\\\":\\\"center\\\",\\\"axis\\\":\\\"vertical\\\",\\\"horizontalAlignment\\\":\\\"center\\\",\\\"distribution\\\":\\\"fill\\\",\\\"verticalAlignment\\\":\\\"center\\\",\\\"children\\\":[{\\\"type\\\":\\\"SpacingView\\\",\\\"content\\\":{\\\"size\\\":\\\"m\\\"}},{\\\"type\\\":\\\"IconView\\\",\\\"content\\\":{\\\"icon\\\":{\\\"name\\\":\\\"{{ parent.icon | default('face_woman') }}\\\"},\\\"shape\\\":\\\"noShape\\\",\\\"size\\\":\\\"large\\\"}},{\\\"type\\\":\\\"SpacingView\\\",\\\"content\\\":{\\\"size\\\":\\\"m\\\"}},{\\\"type\\\":\\\"TextLabel\\\",\\\"content\\\":{\\\"text\\\":\\\"{{ parent.name }}\\\"}},{\\\"type\\\":\\\"SpacingView\\\",\\\"content\\\":{\\\"size\\\":\\\"s\\\"}},{\\\"type\\\":\\\"TextLabel\\\",\\\"content\\\":{\\\"text\\\":\\\"Имеет доступ\\\",\\\"color\\\":\\\"textPositive\\\",\\\"typography\\\":\\\"BodyPrimarySmall\\\"}},{\\\"type\\\":\\\"SpacingView\\\",\\\"content\\\":{\\\"size\\\":\\\"l\\\"}},{\\\"type\\\":\\\"ButtonView\\\",\\\"content\\\":{\\\"text\\\":\\\"Отключить доступ\\\",\\\"style\\\":\\\"secondary\\\",\\\"icon\\\":{\\\"name\\\":\\\"glyph_lock-closed_m\\\"},\\\"action\\\":{\\\"type\\\":\\\"http\\\",\\\"endpoint\\\":\\\"/delete-parent\\\",\\\"method\\\":\\\"post\\\",\\\"body\\\":{\\\"bodyType\\\":\\\"application/json\\\",\\\"values\\\":[{\\\"id\\\":\\\"{{ parent.id }}\\\"}]},\\\"analytics\\\":{\\\"label\\\":\\\"Disable access\\\",\\\"category\\\":\\\"Parent control\\\",\\\"dimensions\\\":[],\\\"action\\\":\\\"Click\\\",\\\"screen\\\":\\\"List parents\\\"}}}},{\\\"type\\\":\\\"SpacingView\\\",\\\"content\\\":{\\\"size\\\":\\\"l\\\"}}]}},\\\"style\\\":{\\\"backgroundColor\\\":\\\"transparent\\\",\\\"style\\\":\\\"shadow\\\"}},{\\\"type\\\":\\\"SpacingView\\\",\\\"content\\\":{\\\"size\\\":\\\"xs\\\"}},}{%ifnotloop.last%},{%endif%}{%endfor%}]},\\\"footer\\\":{\\\"type\\\":\\\"ButtonView\\\",\\\"content\\\":{\\\"padding\\\":{\\\"top\\\":\\\"m\\\",\\\"right\\\":\\\"m\\\",\\\"left\\\":\\\"m\\\",\\\"bottom\\\":\\\"m\\\"},\\\"text\\\":\\\"Добавить родителя\\\",\\\"size\\\":\\\"large\\\",\\\"style\\\":\\\"primary\\\",\\\"action\\\":{\\\"type\\\":\\\"deeplink\\\",\\\"url\\\":\\\"/portfolio/parent-control/add-parent\\\",\\\"analytics\\\":{\\\"label\\\":\\\"Add second parent\\\",\\\"category\\\":\\\"Parent control\\\",\\\"dimensions\\\":[],\\\"action\\\":\\\"Click\\\",\\\"screen\\\":\\\"List parents\\\"}}}},\\\"analytics\\\":{\\\"label\\\":\\\"Screen list\\\",\\\"category\\\":\\\"Parent control\\\",\\\"dimensions\\\":[],\\\"action\\\":\\\"Load\\\",\\\"screen\\\":\\\"List parents\\\"}}"
         )
     })
 })
