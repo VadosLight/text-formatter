a14dd8863f53fff5fecc228b1ca74e58b21ccc81
Vadim Shot <it-vadim@inbox.ru>
2026-03-02 14:42:22 +0300

    Updates

--- assets/index-JniZLHR4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CBAZA3Qx.js";import{P as e}from"./PageWrapper-DAKekbDO.js";const a=()=>r.jsx(e,{children:"Главная страничка"});export{a as default};
---
+++ assets/index-OaaKJR3W.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DZOazwrQ.js";import{P as e}from"./PageWrapper-Cy-iCS2i.js";const a=()=>r.jsx(e,{children:"Главная страничка"});export{a as default};
